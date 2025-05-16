37faf01a591cc7d0de7d58a324f0e6eba4779389
Sándor Balázs <sabuaai@INF.ELTE.HU>
2025-05-16 12:21:57 +0200

    Com Working 3

--- elte-submarine-com/elte-submarine-recive/RS485Recive.h
+++ elte-submarine-com/elte-submarine-recive/RS485Recive.h
@@ -1,21 +1,44 @@
+#ifndef RS485RECEIVE_H
+#define RS485RECEIVE_H
+
 #include <SoftwareSerial.h>
 
 class RS485Receive {
 private:
     SoftwareSerial rs485Serial;
+    int    joyX;
+    int    joyY;
+    int buttonS;
+    int buttonA;
+    int buttonB;
+    int buttonC;
+    int buttonD;
+    int buttonE;
+    int buttonF;
 
 public:
+
     RS485Receive(int rxPin, int txPin)
         : rs485Serial(rxPin, txPin) {}
+
+    int getX() const { return joyX; }
+    int getY() const { return joyY; }
+
+    bool isSPressed() const { return buttonS; }
+    bool isAPressed() const { return buttonA; }
+    bool isBPressed() const { return buttonB; }
+    bool isCPressed() const { return buttonC; }
+    bool isDPressed() const { return buttonD; }
+    bool isEPressed() const { return buttonE; }
+    bool isFPressed() const { return buttonF; }
+
 
     void init(long baudrate = 9600) {
         rs485Serial.begin(baudrate);
     }
 
     void update() {
-        Serial.print("started");
         if (rs485Serial.available()) {
-            Serial.print("available");
             String message = rs485Serial.readStringUntil('\n');
             message.trim(); // felesleges whitespace eltávolítása
 
@@ -36,16 +59,31 @@ public:
                 lastIndex = index + 1;
             }
 
-            // Kiírás a soros monitorra
-            Serial.print("X: "); Serial.print(values[0]);
-            Serial.print(" | Y: "); Serial.print(values[1]);
-            Serial.print(" | SW: "); Serial.print(values[2]);
-            Serial.print(" | A: "); Serial.print(values[3]);
-            Serial.print(" | B: "); Serial.print(values[4]);
-            Serial.print(" | C: "); Serial.print(values[5]);
-            Serial.print(" | D: "); Serial.print(values[6]);
-            Serial.print(" | E: "); Serial.print(values[7]);
-            Serial.print(" | F: "); Serial.println(values[8]);
+            if(false){
+              Serial.print("X: "); Serial.print(values[0]);
+              Serial.print(" | Y: "); Serial.print(values[1]);
+              Serial.print(" | SW: "); Serial.print(values[2]);
+              Serial.print(" | A: "); Serial.print(values[3]);
+              Serial.print(" | B: "); Serial.print(values[4]);
+              Serial.print(" | C: "); Serial.print(values[5]);
+              Serial.print(" | D: "); Serial.print(values[6]);
+              Serial.print(" | E: "); Serial.print(values[7]);
+              Serial.print(" | F: "); Serial.println(values[8]);
+            }
+
+               joyX = values[0];
+               joyY = values[1];
+            buttonS = values[2];
+            buttonA = values[3];
+            buttonB = values[4];
+            buttonC = values[5];
+            buttonD = values[6];
+            buttonE = values[7];
+            buttonF = values[8];
+        }else{
+            Serial.println("RS485 NOT AVAILABLE!");
         }
     }
 };
+
+#endif
--- elte-submarine-com/elte-submarine-recive/elte-submarine-recive.ino
+++ elte-submarine-com/elte-submarine-recive/elte-submarine-recive.ino
@@ -1,14 +1,33 @@
 #include "RS485Recive.h"
+#include "MotorController.h"
+#include "JoystickInputRS485.h"
 
-RS485Receive serialRS485 = RS485Receive(13,12);
+JoystickInputRS485 joystic = JoystickInputRS485();
+MotorController motor = MotorController(9, 8);
 
 void setup() {
     Serial.begin(9600);
-    serialRS485.init();
+    joystic.init();
+    motor.init();
 }
 
 void loop() {
-  Serial.println("Recive...");
-  serialRS485.update();
-  delay(200);
+    joystic.update();
+
+    int joyX = joystic.getX();
+    //Serial.println(joyX);
+    int joyY = joystic.getY();
+
+    motor.processJoystickInput(joyX, joyY);
+    /*if(joystic.isDPressed()) motor.leftTurn();
+    if(joystic.isDReleased()) motor.reset();
+    if(joystic.isBPressed()) motor.rightTurn();
+    if(joystic.isBReleased()) motor.reset();*/
+
+    Serial.print("LEF: ");
+    Serial.print(motor.getLeftSpeed());
+    Serial.print(" | RIG: ");
+    Serial.println(motor.getRightSpeed());
+
+    delay(200);
 }
